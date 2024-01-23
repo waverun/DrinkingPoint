bc0ddaf5c0235bb2316c69612e61ac564ffbfcb9
Shay Moreno <moa129@gmail.com>
2024-01-23 13:59:20 +0200

    first commit

---
+++ DrinkingPoint/Classes/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  DrinkingPoint
+//
+//  Created by shay moreno on 22/01/2024.
+//
+
+import Foundation
---
+++ DrinkingPoint/Views/MapView.swift
@@ -0,0 +1,8 @@
+//
+//  MapView.swift
+//  DrinkingPoint
+//
+//  Created by shay moreno on 21/01/2024.
+//
+
+import Foundation
