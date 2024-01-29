428b738a475ca6afe838b5558fe7ed2d0382d7f0
Shay Moreno <moa129@gmail.com>
2024-01-29 13:51:17 +0200

    add firebase analytics

--- DrinkingPoint/Classes/AppDelegate.swift
+++ DrinkingPoint/Classes/AppDelegate.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 import FirebaseCore
+import FirebaseAnalytics
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        Analytics.setAnalyticsCollectionEnabled(true)
         return true
     }
 }
