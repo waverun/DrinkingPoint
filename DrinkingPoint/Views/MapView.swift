ef89bf1e3a5cd7291ee616037f9ba7155020128f
Shay Moreno <moa129@gmail.com>
2024-01-31 17:56:43 +0200

    blue water point

--- DrinkingPoint/Views/MapView.swift
+++ DrinkingPoint/Views/MapView.swift
@@ -18,11 +18,20 @@ import MapKit
 //        }
 //}
 
-class MapViewManager {
+class MapViewManager: NSObject, MKMapViewDelegate {
     static let shared = MapViewManager()
-    private init() {}
 
-    let mapView = MKMapView(frame: .zero)
+    private override init() {}
+
+//    let mapView = MKMapView(frame: .zero)
+
+    lazy var mapView: MKMapView = {
+        let map = MKMapView(frame: .zero)
+        map.delegate = self // Set delegate to self
+        map.showsUserLocation = true
+        // Other setup...
+        return map
+    }()
 
     func makeMapView() -> MKMapView {
         mapView.showsUserLocation = true
@@ -49,6 +58,78 @@ class MapViewManager {
         annotation.title = title
         mapView.addAnnotation(annotation)
     }
+
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        // Check if the annotation is not the user's location
+//        if annotation is MKUserLocation {
+//            return nil
+//        }
+//
+//        let identifier = "Annotation"
+//        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+//
+//        if annotationView == nil {
+//            // Create a new annotation view
+//            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+//            annotationView?.canShowCallout = true // If you want to show a callout bubble
+//
+//            // Set the image for the annotation view using a system image
+//            let waterSymbol = UIImage(systemName: "drop.fill")
+//
+//            // Specify the light blue color you want to use
+//            let lightBlueColor = UIColor.systemBlue.withAlphaComponent(0.5) // Adjust alpha for lighter shade
+//
+//            // Apply the tint color to the image
+//            let lightBlueWaterSymbol = waterSymbol?.withTintColor(lightBlueColor, renderingMode: .alwaysTemplate)
+//
+//            let image = lightBlueWaterSymbol // ?.withTintColor(lightBlueColor)
+//
+//            annotationView?.image = image
+//
+//        } else {
+//            // Reuse the annotation view
+//            annotationView?.annotation = annotation
+//        }
+//
+//        return annotationView
+//    }
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // Check if the annotation is not the user's location
+        if annotation is MKUserLocation {
+            return nil
+        }
+
+        let identifier = "Annotation"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+
+        if annotationView == nil {
+            // Create a new annotation view
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true // If you want to show a callout bubble
+
+            // Create an UIImageView for the annotation view
+            let imageView = UIImageView(frame: CGRect(x: -10, y: -10, width: 20, height: 20)) // Adjust size as needed
+            imageView.contentMode = .scaleAspectFit
+
+            // Set the image for the image view using a system image
+            let waterSymbol = UIImage(systemName: "drop.fill")
+            imageView.image = waterSymbol
+
+            // Specify the light blue color you want to use and apply it to the image view
+            let lightBlueColor = UIColor.systemBlue.withAlphaComponent(0.75) // Adjust alpha for lighter shade
+            imageView.tintColor = lightBlueColor
+
+            // Add the image view to the annotation view
+            annotationView?.addSubview(imageView)
+        } else {
+            // Reuse the annotation view
+            annotationView?.annotation = annotation
+        }
+
+        return annotationView
+    }
+
 }
 
 struct MapView: UIViewRepresentable {
