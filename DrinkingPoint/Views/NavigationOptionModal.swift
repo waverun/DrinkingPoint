1c19936640767c31366201dab178e44929dcbdce
Shay Moreno <moa129@gmail.com>
2024-02-14 22:15:39 +0200

    blur view

--- DrinkingPoint/Views/NavigationOptionModal.swift
+++ DrinkingPoint/Views/NavigationOptionModal.swift
@@ -6,67 +6,70 @@ struct NavigationOptionModal: View {
 
     var body: some View {
         VStack {
-            Text("Navigate to:")
-                .font(.system(size: 18, weight: .bold)) // Adjust the font size as needed
-                .padding()
+            ZStack {
+//                 Use BlurView as the background
+                BlurView(style: .systemMaterial) // You can choose different styles like .dark, .light, etc.
+//                .padding()
+                .frame(width: 250, height: 300)
+                .cornerRadius(12)
+                .shadow(radius: 8)
 
-            HStack {
-                // AsyncImage introduced in iOS 15.0 for async loading.
-                if let imageURL = annotation.imageURL, let url = URL(string: imageURL) {
-                    AsyncImage(url: url) { image in
-                        image.resizable()
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    .frame(width: 100, height: 100)
-                    .cornerRadius(8)
-                }
-
-                // Title
-                if let title = annotation.title {
-                    Text(title)
-                        .font(.headline)
+                VStack {
+                    Text("Navigate to:")
+                        .font(.system(size: 18, weight: .bold)) // Adjust the font size as needed
                         .padding()
+
+                    HStack {
+                        // AsyncImage introduced in iOS 15.0 for async loading.
+                        if let imageURL = annotation.imageURL, let url = URL(string: imageURL) {
+                            AsyncImage(url: url) { image in
+                                image.resizable()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 100, height: 100)
+                            .cornerRadius(8)
+                        }
+
+                        // Title
+                        if let title = annotation.title {
+                            Text(title)
+                                .font(.headline)
+                                .padding()
+                        }
+                    }
+
+                    Divider()
+
+                    // Navigation Buttons
+                    Button("Open in Apple Maps") {
+                        openInAppleMaps(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
+                        isPresented = false
+                    }
+
+                    Divider()
+
+                    Button("Open in Google Maps") {
+                        openInGoogleMaps(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
+                        isPresented = false
+                    }
+
+                    Divider()
+
+                    Button("Cancel") {
+                        isPresented = false
+                    }
                 }
+
+                .padding()
+                .frame(width: 250, height: 300)
+//                .background(Color.white)
+//                .cornerRadius(12)
+//                .shadow(radius: 8)
+                .foregroundColor(.blue)
             }
-
-            Divider()
-
-            // Navigation Buttons
-            Button("Open in Apple Maps") {
-                openInAppleMaps(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
-                isPresented = false
-            }
-
-            Divider()
-
-            Button("Open in Google Maps") {
-                openInGoogleMaps(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
-                isPresented = false
-            }
-
-            Divider()
-
-            Button("Cancel") {
-                isPresented = false
-            }
+            // Use clipShape to apply the rounded corners to the entire ZStack, including the BlurView
+            .clipShape(RoundedRectangle(cornerRadius: 12))
         }
-//        .padding()
-//        .frame(width: 250, height: 300)
-//        .background(Color.white.opacity(0.6)) // Semi-transparent white background
-//        .cornerRadius(12)
-//        .shadow(radius: 8)
-//        .foregroundColor(.blue)
-//        // The following modifier centers the modal in the ZStack.
-//        .frame(maxWidth: .infinity, maxHeight: .infinity)
-//        .background(Color.black.opacity(0.4)) // Semi-transparent background for the rest of the screen
-//        .edgesIgnoringSafeArea(.all) // Makes the semi-transparent background extend to the edges
-
-        .padding()
-        .frame(width: 250, height: 300)
-        .background(Color.white)
-        .cornerRadius(12)
-        .shadow(radius: 8)
-        .foregroundColor(.blue)
     }
 }
